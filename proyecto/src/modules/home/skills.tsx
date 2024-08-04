7288610010b15d8b93a9a02883d603dd97dbaeb8
Jareth <jmenal@ucenfotec.ac.cr>
2024-08-04 11:26:24 -0600

    fixed ts err

--- proyecto/src/modules/home/skills.tsx
+++ proyecto/src/modules/home/skills.tsx
@@ -1,9 +1,9 @@
 import React, { memo } from 'react';
 import SkillCard from "../utilities/skill-card.tsx";
-import {HomeProps} from "../types/HomeTypes.tsx";
+import {SkillCategory} from "../types/ComponentTypes.tsx";
 
 
-const Skills: React.FC<HomeProps> = memo(({ t, skills }) => {
+const Skills: React.FC<{t: (key: string) => string, skills: SkillCategory[]}> = memo(({ t, skills }) => {
     return (
         <div className="flex flex-col p-10">
             <h1 className="pb-5 text-mena-sun dark:text-mena-sky text-8xl max-[800px]:text-6xl font-extrabold">{t('sectionTitle.skills')}</h1>
